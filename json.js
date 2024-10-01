79f38ba00fe8ff24459419335ce5b9156dca2271
Yasin Arafat <yasinarafat2557@gmail.com>
2024-10-01 16:34:26 +0600

    json parse and stringify

---
+++ json.js
@@ -0,0 +1,27 @@
+const user = {id: 1, name: "Gorib Amir", job: 'actor'};
+// JavaScript Object Notation (JSON)
+const stringifyUser = JSON.stringify(user);
+console.log(stringifyUser); // {"id":1,"name":"Gorib Amir","job":"actor"}
+
+
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: "kochukhet voot er goli",
+        city: 'ranbir',
+        country: 'BD'
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+} 
+console.log(shop);
+// convert to string using JSON
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+
+// convert to Object from JSON string
+const shopObject = JSON.parse(shopJSON)
+console.log(shopObject);
