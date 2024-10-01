77a1909c18608e4300cd7f3133e203dd11fed85e
Yasin Arafat <yasinarafat2557@gmail.com>
2024-10-02 00:49:07 +0600

    creating post method and adding loading behavior when try to post

---
+++ JS/createPost.js
@@ -0,0 +1,54 @@
+
+
+const loader = document.getElementById('loader');
+
+const showLoading = () => {
+    loader.classList.remove('hidden'); 
+}
+
+const hideLoading = () => {
+    loader.classList.add('hidden');
+}
+
+
+const newPost = {
+    title: "foo",
+    body: "bar",
+    userId: 11
+}
+
+/*
+    1. get the container element where you want to add the new element
+    2. create child element
+    3. set innerText or innerHTML
+    4. appendChild
+*/
+
+function createNewPost(){
+    showLoading()
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: {
+            'Content-type' : 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(data => displayNewPost(data))
+}
+
+function displayNewPost(newPost){
+
+    const postsContainer = document.getElementById('postsContainer');
+    // console.log(newPost)
+    const article = document.createElement('article');
+        article.classList.add('newPost')
+        article.innerHTML = `
+            <h3>User-${newPost.userId}</h3>
+            <h3>${newPost.title}</h3>
+            <p>${newPost.body}</p> 
+        `
+        postsContainer.insertAdjacentElement('afterbegin', article)
+    
+    hideLoading()
+}
